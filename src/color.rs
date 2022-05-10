f6c9c621cba410231306775fc18ee2dcf1206466
David Biró <dbiro97@gmail.com>
2022-05-10 15:49:39 +0200

    Change colors

--- src/color.rs
+++ src/color.rs
@@ -3,17 +3,15 @@
 // win.rs).  The foreground/background and cursor get there own 'slots' after
 // the 256 color palette.
 pub const COLOR_NAMES: &[&str] = &[
-    /* 8 normal colors */
-    "black", "red3", "green3", "yellow3", "blue2", "magenta3", "cyan3", "gray90",
-    /* 8 bright colors */
-    "gray50", "red", "green", "yellow", "#5c5cff", "magenta", "cyan", "white",
+    "#3f3f3f", "#705050", "#60b48a", "#dfaf8f", "#506070", "#dc8cc3", "#8cd0d3", "#dcdccc",
+    "#709080", "#dca3a3", "#c3bf9f", "#f0dfaf", "#94bff3", "#ec93d3", "#93e0e3", "#ffffff",
 ];
 
 pub const FG_COLOR: usize = 258;
 pub const BG_COLOR: usize = 259;
 pub const CURSOR_COLOR: usize = 256;
 pub const CURSOR_REV_COLOR: usize = 257;
-pub const FG_COLOR_NAME: &str = "grey90";
-pub const BG_COLOR_NAME: &str = "black";
+pub const FG_COLOR_NAME: &str = "#ffffff";
+pub const BG_COLOR_NAME: &str = "#101316";
 pub const CURSOR_COLOR_NAME: &str = "#cccccc";
 pub const CURSOR_REV_COLOR_NAME: &str = "#555555";
